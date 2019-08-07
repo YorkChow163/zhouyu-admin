b4fee1f0128492d350a6c9b16bf4504cf8004514
yorkchow163 <2171677178@qq.com>
2019-08-07 16:08:53 +0800

    springcloud alibaba服务提供和消费

--- consumer-demo/src/main/java/com/zhouyu/consumer/controller/UserController.java
+++ consumer-demo/src/main/java/com/zhouyu/consumer/controller/UserController.java
@@ -1,29 +1,31 @@
 package com.zhouyu.consumer.controller;
 
-import com.zhouyu.provider.api.UserLoginApi;
+import com.zhouyu.dubbo.api.UserServiceApi;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 周宇
  * @version 1.0 date: 2019/8/2 14:55
- * @description:
+ * @description: 服务消费者
  * @Copyright (C), 2017-2019, 深圳市⾦证引擎科技股份有限公司
  */
-@Controller
+@RestController
+@RequestMapping("/user")
 @Log4j2
 public class UserController {
 
-  @Autowired
-  private UserLoginApi userLoginApi;
+  @Reference
+  UserServiceApi userServiceApi;
 
-  @RequestMapping(value = "/user", method = RequestMethod.GET)
-  public void getUserInfo() {
-    String user = userLoginApi.getUser();
-    log.info("provider,user:{}", user);
+  @RequestMapping(value = "/infos/name", method = RequestMethod.GET)
+  public void getUserInfo(@PathVariable("name") String name) {
+    String providerReturn = userServiceApi.echo(name);
+    log.info("消费者,response:{}", providerReturn);
   }
 
 }
